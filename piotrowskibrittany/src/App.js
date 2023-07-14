54983199681b61e45d303250f8364642f0c779c7
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-07-14 14:45:44 -0400

    empty commit

--- piotrowskibrittany/src/App.js
+++ piotrowskibrittany/src/App.js
@@ -17,7 +17,6 @@ function App(){
           {/* <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/Home" element={<Home />}/>
-            
           </Routes> */}
           <Home />
         </div>
